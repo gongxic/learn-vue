4b88d13e9b7e25adcce833d6e7770ee8febc507d
Xicheng.Gong <xicheng.gong@accenture.com>
2020-08-04 10:56:37 +0800

    change

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
-
-Vue.use(VueRouter)
